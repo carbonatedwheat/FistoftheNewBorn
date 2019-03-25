fdafa1bb8e620a47df0d39b4166f7aa198f61239
RooStrom <roostrom64@gmail.com>
2019-03-25 13:33:02 -0700

    camera

---
+++ Repo/Q4P/Assets/Scripts/CamMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    public GameObject target;
+    public float rotateSpeed = 5;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = target.transform.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.transform.Rotate(0, horizontal, 0);
+
+        //float Vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        //target.transform.Rotate(Vertical, 0, 0);
+
+        //float DesiredAngle = target.transform.eulerAngles.x;
+        //Quaternion rotation = Quaternion.Euler(DesiredAngle, 0, 0);
+        //transform.position = target.transform.position - (rotation * offset);
+
+        float desiredAngle = target.transform.eulerAngles.y;
+        Quaternion rotation1 = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = target.transform.position - (rotation1 * offset);
+
+        transform.LookAt(target.transform);
+    }
+}
