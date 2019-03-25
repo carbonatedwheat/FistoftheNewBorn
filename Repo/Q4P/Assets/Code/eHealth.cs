964aa8a1c0b1848d0ba0bae63885e199648af39d
Johnathan Blaikie <johnathanblaikie@gmail.com>
2019-03-25 13:29:13 -0700

    Added Folders, a Scene, Tags, and Enemy Health

---
+++ Repo/Q4P/Assets/Code/eHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eHealth : MonoBehaviour {
+
+    public float eMaxHealth;
+    public float eMinHealth;
+    public float eCurrentHealth;
+    public GameObject Host;
+	// Use this for initialization
+	void Start () {
+        eCurrentHealth = eMaxHealth;
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Hitbox")
+        {
+            eCurrentHealth--;
+            //eCurrentHealth - damage = eCurrentHealth;
+            Debug.Log("Checkem");
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		if(eCurrentHealth <= eMinHealth)
+        {
+            Host.SetActive(false);
+        }
+	}
+}
