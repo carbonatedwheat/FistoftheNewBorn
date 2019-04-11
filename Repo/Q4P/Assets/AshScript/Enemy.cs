30bf1e4fb7c84120355339b0fe32634293d9ade1
Johnathan Blaikie <johnathanblaikie@gmail.com>
2019-04-11 13:26:54 -0700

    Summary

--- Repo/Q4P/Assets/AshScript/Enemy.cs
+++ Repo/Q4P/Assets/AshScript/Enemy.cs
@@ -4,43 +4,33 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    Animator anim;
+    public Animator anim;
     public float speed;
     public float Max;
     public float Min;
-    public float aRange;
     public Transform Player;
-
 
     void Start()
     {
         
         Player = GameObject.FindWithTag("Player").transform;
-        anim.GetComponentInChildren<Animator>();
+        anim = GetComponent<Animator>();
+        
     }
 
     void Update()
     {
-
-
         transform.LookAt(Player);
-
-
+        
         if (Vector3.Distance(transform.position, Player.position) >= Min)
         {
             transform.position += transform.forward * speed * Time.deltaTime;
+           
         }
         if (Vector3.Distance(transform.position, Player.position) <= Max)
         {
-            
+          
         }
-        if(Vector3.Distance(transform.position, Player.position) >= aRange)
-        {
-            anim.SetBool("Attack", true);
-        }
-        else
-        {
-            anim.SetBool("Attack", false);
-        }
+       
     }
 }
