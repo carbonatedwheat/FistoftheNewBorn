eaf9a7f6b7cf1f3b63307c093681c36f0337ce5a
RooStrom <roostrom64@gmail.com>
2019-04-23 13:06:36 -0700

    HealthBar+Finally FUcking Fixing unity

--- Repo/Q4P/Assets/Code/Damage and Health/pHealth.cs
+++ Repo/Q4P/Assets/Code/Damage and Health/pHealth.cs
@@ -1,149 +1,111 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class pHealth : MonoBehaviour
-{
-    public float pMaxHealth;
-    public float pMinHealth;
-    public float pCurrentHealth;
-    public bool pBlock;
-    public pAttack pAttack;
-    public DamageScript damage;
-    // Start is called before the first frame update
-    void Start()
-    {
-        pCurrentHealth = pMaxHealth;
-    }
+//public class pHealth : MonoBehaviour
+//{
+//    //public float pMaxHealth;
+//    //public float pMinHealth;
+//    //public float pCurrentHealth;
+//    //public bool pBlock;
+//    //public pAttack pAttack;
+//    //public DamageScript damage;
+//    //// Start is called before the first frame update
+//    //void Start()
+//    //{
+//    //    pCurrentHealth = pMaxHealth;
+//    //}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (pBlock == false)
-        {
-            if (other.gameObject.tag == "HurtBox")
-            {
-                pCurrentHealth -= damage.killZone;
-            }
-            if (other.gameObject.tag == "SpiderStab")
-            {
-                pCurrentHealth -= damage.spiderStab;
+//    //private void OnTriggerEnter(Collider other)
+//    //{
+//    //    if (pBlock == false)
+//    //    {
+//    //        if (other.gameObject.tag == "HurtBox")
+//    //        {
+//    //            pCurrentHealth -= damage.killZone;
+//    //        }
+//    //        if (other.gameObject.tag == "SpiderStab")
+//    //        {
+//    //            pCurrentHealth -= damage.spiderStab;
 
-            }
-            if (other.gameObject.tag == "AntSlash")
-            {
-                pCurrentHealth -= damage.samuraiAntSlash;
+//    //        }
+//    //        if (other.gameObject.tag == "AntSlash")
+//    //        {
+//    //            pCurrentHealth -= damage.samuraiAntSlash;
 
-            }
-        }
-        if (pBlock == true)
-        {
-            if (other.gameObject.tag == "BlockSpell")
-            {
-                pCurrentHealth -= damage.spellingBlockSpell * 0.5f;
+//    //        }
+//    //    }
+//    //    if (pBlock == true)
+//    //    {
+//    //        if (other.gameObject.tag == "BlockSpell")
+//    //        {
+//    //            pCurrentHealth -= damage.spellingBlockSpell * 0.5f;
 
-            }
-            if (other.gameObject.tag == "SpiderStab")
-            {
-                pCurrentHealth -= damage.spiderStab * 0.5f;
+//    //        }
+//    //        if (other.gameObject.tag == "SpiderStab")
+//    //        {
+//    //            pCurrentHealth -= damage.spiderStab * 0.5f;
 
-            }
-            if (other.gameObject.tag == "AntSlash")
-            {
-                pCurrentHealth -= damage.samuraiAntSlash * 0.5f;
-                Debug.Log("Reduced Damage");
-            }
-            if (other.gameObject.tag == "BlockSpell")
-            {
-                pCurrentHealth -= damage.spellingBlockSpell * 0.5f;
+//    //        }
+//    //        if (other.gameObject.tag == "AntSlash")
+//    //        {
+//    //            pCurrentHealth -= damage.samuraiAntSlash * 0.5f;
+//    //            Debug.Log("Reduced Damage");
+//    //        }
+//    //        if (other.gameObject.tag == "BlockSpell")
+//    //        {
+//    //            pCurrentHealth -= damage.spellingBlockSpell * 0.5f;
 
-            }
-        }
-    }
+//    //        }
+//    //    }
+//    //}
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    if (collision.gameObject.tag == "HurtBox")
-    //    {
-    //        pCurrentHealth -= damage.enemy;
+//    ////private void OnCollisionEnter(Collision collision)
+//    ////{
+//    ////    if (collision.gameObject.tag == "HurtBox")
+//    ////    {
+//    ////        pCurrentHealth -= damage.enemy;
 
-    //    }
-    //    if (collision.gameObject.tag == "SpiderStab")
-    //    {
-    //        pCurrentHealth -= damage.spiderStab;
+//    ////    }
+//    ////    if (collision.gameObject.tag == "SpiderStab")
+//    ////    {
+//    ////        pCurrentHealth -= damage.spiderStab;
 
-    //    }
-    //    if (collision.gameObject.tag == "AntSlash")
-    //    {
-    //        pCurrentHealth -= damage.samuraiAntSlash;
+//    ////    }
+//    ////    if (collision.gameObject.tag == "AntSlash")
+//    ////    {
+//    ////        pCurrentHealth -= damage.samuraiAntSlash;
 
-    //    }
-    //    if (collision.gameObject.tag == "BlockSpell")
-    //    {
-    //        pCurrentHealth -= damage.spellingBlockSpell;
+//    ////    }
+//    ////    if (collision.gameObject.tag == "BlockSpell")
+//    ////    {
+//    ////        pCurrentHealth -= damage.spellingBlockSpell;
 
-    //    }
-    //}
-    // Update is called once per frame
-    void Update()
-    {
-        if (pCurrentHealth <= pMinHealth)
-        {
-            //Debug.Log("Game Over");
-            SceneManager.LoadScene("GameOver");
-            gameObject.SetActive(false);
+//    ////    }
+//    ////}
+//    //// Update is called once per frame
+//    //void Update()
+//    //{
+//    //    if (pCurrentHealth <= pMinHealth)
+//    //    {
+//    //        //Debug.Log("Game Over");
+//    //        SceneManager.LoadScene("GameOver");
+//    //        gameObject.SetActive(false);
 
 
 
-        }
+//    //    }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
->>>>>>> parent of b4130e9... save
-=======
->>>>>>> parent of b4130e9... save
-        if (pAttack.blockerBool == true)
-        {
-            pBlock = true;
-        }
-        if (pAttack.blockerBool == false)
-        {
-            pBlock = false;
-        }
-<<<<<<< HEAD
-        
-=======
+//    //    if (pAttack.blockerBool == true)
+//    //    {
+//    //        pBlock = true;
+//    //    }
+//    //    if (pAttack.blockerBool == false)
+//    //    {
+//    //        pBlock = false;
+//    //    }
 
-=======
-        //if (pAttack.blockerBool == true)
-        //{
-        //    pBlock = true;
-        //}
-        //if(pAttack.blockerBool == false)
-        //{
-        //    pBlock = false;
-        //}
-       
->>>>>>> e7f2df94e69099b15b1faf2fbaeb4d099b5f3c57
-
-<<<<<<< HEAD
->>>>>>> parent of b4130e9... save
-=======
-=======
-        //if (pAttack.blockerBool == true)
-        //{
-        //    pBlock = true;
-        //}
-        //if(pAttack.blockerBool == false)
-        //{
-        //    pBlock = false;
-        //}
-       
->>>>>>> e7f2df94e69099b15b1faf2fbaeb4d099b5f3c57
-
->>>>>>> parent of b4130e9... save
-    }
-}
+//    //}
+//}
