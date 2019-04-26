11cee908d218a9bd70192f00005a481168203c2a
RooStrom <roostrom64@gmail.com>
2019-04-26 12:48:22 -0700

    takethisbitch YEET

---
+++ Repo/Q4P/Assets/BarzOfHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarzOfHealth : MonoBehaviour
+{
+    public pGeneral G;
+    public Transform bar;
+
+
+    void Start()
+    {
+        bar = transform.Find("Bar");
+        
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+        bar.localScale = new Vector3(1f, sizeNormalized);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bar.localScale = new Vector3(1f, G.pCurrentHealth* .01f);
+    }
+}
