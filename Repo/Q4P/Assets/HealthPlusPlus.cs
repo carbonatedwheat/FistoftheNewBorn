8f2be7d672e60dc6ff8c2430c27c54b9cf75df89
RooStrom <roostrom64@gmail.com>
2019-05-02 12:55:17 -0700

    jbhjbdfj,b

---
+++ Repo/Q4P/Assets/HealthPlusPlus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPlusPlus : MonoBehaviour
+{
+    public float speed = 10f;
+    public float xspeed = 5f;
+    float height = 0.25f;
+    public pGeneral G;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+
+        Vector3 pos = transform.position;
+       
+        float newY = Mathf.Sin(Time.time * xspeed) * height + pos.y;
+       
+        transform.position = new Vector3(pos.x, newY, pos.z) * height;
+    }
+}
