706a3899970204c5afffa5c6b703a3c19d6aa06a
RooStrom <roostrom64@gmail.com>
2019-05-06 20:04:22 -0700

    FUCKINBITCHASSSCRIPTS

---
+++ Repo/Q4P/Assets/PlayGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayGame : MonoBehaviour
+{
+    public float delay = 440;
+    public string NewLevel = "GameOver";
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(NewLevel);
+    }
+}
