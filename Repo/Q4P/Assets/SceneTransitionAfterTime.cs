385c9ed41cf143396231f2d862458e02b6c07f30
RooStrom <roostrom64@gmail.com>
2019-05-08 13:39:51 -0700

    xdfbgfb

---
+++ Repo/Q4P/Assets/SceneTransitionAfterTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionAfterTime : MonoBehaviour
+{
+    public float delay = 10;
+    public string NewLevel = "ActualLevel";
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay)); 
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(NewLevel);
+
+    }
+   
+}
